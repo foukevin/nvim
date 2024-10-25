8b7b2dba37e889580bd884156d300035974c00d2
Kévin Delbrayelle <kevin.delbrayelle@gmail.com>
2024-10-25 15:55:04 +0200

    Reorder options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,5 @@
 local options = {
-    -- line numbering
+    -- line numbering, column coloring and scrolling
     number          = true,     -- Print the line number in front of each line
     relativenumber  = false,    -- Show the line number relative to the line
                                 -- with the cursor in front of each line
@@ -7,50 +7,45 @@ local options = {
                                 -- keep above and below the cursor
     showmode        = false,    -- If in Insert, Replace or Visual mode
                                 -- put a message on the last line
+    colorcolumn     = "100",    -- 'colorcolumn' is a comma-separated list
 
 
-    backspace       = "indent,eol,start",
     swapfile        = false,
 
     -- tabs and indentation
- -- breakindent     = true,     -- Every wrapped line will continue visually indented
+    breakindent     = true,     -- Every wrapped line will continue visually indented
                                    -- (same amount of space as the beginning of that line)
- -- cindent         = true,     -- Enables automatic C program indenting
     tabstop         = 4,        -- Number of spaces that a <Tab> in the file counts for
                                 -- number of spaces to insert a <Tab>
     shiftwidth      = 4,        -- Number of spaces to use for each step of (auto)indent
     expandtab       = true,     -- In Insert mode: Use the appropriate
- -- smartindent     = true,     -- Do smart autoindenting when starting a new line
+    -- smartindent     = true,     -- Do smart autoindenting when starting a new line
+    backspace       = "indent,eol,start",
+    cindent         = true,     -- Enables automatic C program indenting
     -- The 'cinoptions' affect the way 'cindent' reindents lines in a C program
     cinoptions      = ":0,(0,g0,t0,l1",
+    -- Influences the working of <BS>, <Del>, CTRL-W and CTRL-U in Insert mode
+
+
+    -- bracket matching
+    showmatch       = true,     -- When a bracket is inserted, briefly jump to the matching one
+    matchtime       = 2,        -- Tenths of a second to show the matching paren
 
 
     -- search settings
     ignorecase      = true,     -- Ignore case in search patterns
     smartcase       = true,     -- Override the 'ignorecase' option if the search
                                 -- pattern contains upper case characters
-    --[[
     hlsearch        = false,    -- When there is a previous search pattern,
                                 -- highlight all its matches
     incsearch       = true,     -- While typing a search command, show where the pattern,
                                 -- as it was typed so far, matches
-    --]]
-
 
     -- colors
-    colorcolumn     = "100",    -- 'colorcolumn' is a comma-separated list
     termguicolors   = true,     -- Enables 24-bit RGB color in the Terminal UI
                                 -- of screen columns that are highlighted
 
 
-    encoding        = "utf8",   -- String-encoding used internally and for RPC communication
-    emoji           = false,    -- When on all Unicode emoji characters
-                                -- are considered to be full width
-    list            = true,     -- Useful to see the difference between tabs
-                                -- and spaces and for trailing blanks
-    matchtime       = 2,        -- Tenths of a second to show the matching paren
-    showbreak       = "…",      -- String to put at the start of lines that have been wrapped
-    showmatch       = true,     -- When a bracket is inserted, briefly jump to the matching one
     wrap            = false,    -- When on, lines longer than the width of the window will wrap
 
     -- A comma-separated list of options for Insert mode completion
@@ -63,6 +58,14 @@ local options = {
     -- to edit a new buffer and when reading a file into an existing buffer
     fileformats     = "unix,dos,mac",
 
+
+    -- Text display
+    encoding        = "utf8",   -- String-encoding used internally and for RPC communication
+    emoji           = false,    -- When on all Unicode emoji characters
+                                -- are considered to be full width
+    list            = true,     -- Useful to see the difference between tabs
+                                -- and spaces and for trailing blanks
+    showbreak       = "…",      -- String to put at the start of lines that have been wrapped
     -- Strings to use in 'list' mode and for the :list command
     listchars       = "tab:▸ ,trail:·,nbsp:␣,extends:⇢,precedes:⇠",
 }
