e8f8ed5ae4a735b1477592910c7647160ca16560
Kévin Delbrayelle <kevin@dotemu.com>
2024-10-30 09:50:38 +0100

    Use 'which-key' to assign keys for auto-session plugin

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -8,9 +8,14 @@ return {
             auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
         })
 
-        local keymap = vim.keymap
-
-        keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
-        keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+        local wk = require("which-key")
+        wk.add({
+            { mode = "n" },
+            { "<Leader>w", group = "Auto-session"},
+            -- restore last workspace session for current directory
+            { "<Leader>wr", "<cmd>SessionRestore<CR>", desc = "Restore session for cwd" },
+            -- save workspace session for current working directory
+            { "<Leader>ws", "<cmd>SessionSave<CR>", desc = "Save session for auto session root dir" },
+        })
     end,
 }
