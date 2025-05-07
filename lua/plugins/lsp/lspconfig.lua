2d3e42f703e7aedfedb5aff0099b16d7c4222b4c
Kévin Delbrayelle <kevin@dotemu.com>
2025-05-07 16:24:31 +0200

    Use vim.diagnostic.config instead of sign_define which is deprecated

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -64,12 +64,24 @@ local config = function()
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
     -- change the diagnostic symbols in the sign column (gutter)
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+    vim.diagnostic.config {
+        signs = {
+            text = {
+                [vim.diagnostic.severity.ERROR] = " ",
+                [vim.diagnostic.severity.WARN] = " ",
+                [vim.diagnostic.severity.INFO] = "󰠠 ",
+                [vim.diagnostic.severity.HINT] = " ",
+            },
+            -- line number highlight
+            numhl = {
+                -- [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+                -- [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+                -- [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+                -- [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+            }
 
+        }
+    }
 
     local lspconfig = require("lspconfig")
 
