50803528e0d65f79ad84a5e6a4e71e6938540744
Kévin Delbrayelle <kevin@dotemu.com>
2025-09-05 11:03:59 +0200

    Add pylst to mason config

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -22,6 +22,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "clangd",
+                "pylsp",
             },
             -- auto-install configured servers (with lspconfig)
             automatic_installation = true,
