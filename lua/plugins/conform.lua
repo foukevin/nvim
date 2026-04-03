58715f37bc38bda9df78d3d3cbf4d29fc410544a
Kévin Delbrayelle <kevin@dotemu.com>
2026-04-03 12:16:53 +0200

    Update conform plugin configuration

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -18,13 +18,18 @@ return {
                 python = { "isort", "black" },
                 c = { "clang_format" },
                 cpp = { "clang_format" },
+                h = { "clang_format" },
                 CMake = { "cmake_format" },
             },
             formatters = {
                 stylua = {
                     append_args = { "--indent-width", "2", "--indent-type", "Spaces" },
                 },
+                clang_format = {
+                    prepend_args = { '--style=file', '--fallback-style=LLVM' },
+                },
             },
+            lsp_fallback = false,
             -- format_on_save = {
             --   lsp_fallback = true,
             --   async = false,
@@ -32,12 +37,8 @@ return {
             -- },
         })
 
-        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            })
+        vim.keymap.set({ "n", "v" }, "<leader>mf", function()
+            conform.format()
         end, { desc = "Format file or range (in visual mode)" })
     end,
 }
