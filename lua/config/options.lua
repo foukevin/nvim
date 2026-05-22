bd560c7906c89259219381bae9c823fb8189b77f
Kévin Delbrayelle <kevin@dotemu.com>
2026-05-22 15:20:54 +0200

    Remove nvim-cmp (now builtin in nvim)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,44 +13,46 @@ local options = {
 
 
     -- tabs and indentation
-    breakindent = true,                     -- Every wrapped line will continue visually indented
+    breakindent       = true,                     -- Every wrapped line will continue visually indented
     -- (same amount of space as the beginning of that line)
-    tabstop     = 4,                        -- Number of spaces that a <Tab> in the file counts for
+    tabstop           = 4,                        -- Number of spaces that a <Tab> in the file counts for
     -- number of spaces to insert a <Tab>
-    shiftwidth  = 4,                        -- Number of spaces to use for each step of (auto)indent
-    expandtab   = true,                     -- In Insert mode: Use the appropriate
+    shiftwidth        = 4,                        -- Number of spaces to use for each step of (auto)indent
+    expandtab         = true,                     -- In Insert mode: Use the appropriate
     -- smartindent     = true,     -- Do smart autoindenting when starting a new line
-    cindent     = true,                     -- Enables automatic C program indenting
-    cinoptions  = ":0,(0,g0,t0,l1,N-s,E-s", -- The 'cinoptions' affect the way 'cindent'
+    cindent           = true,                     -- Enables automatic C program indenting
+    cinoptions        = ":0,(0,g0,t0,l1,N-s,E-s", -- The 'cinoptions' affect the way 'cindent'
     -- reindents lines in a C program
-    backspace   = "indent,eol,start",       -- Influences the working of <BS>, <Del>,
+    backspace         = "indent,eol,start",       -- Influences the working of <BS>, <Del>,
     -- CTRL-W and CTRL-U in Insert mode
 
+    -- autocompletion
+    autocomplete      = true, -- 'nvim-cmp' optional now
+    autocompletedelay = 200,  -- Delay in ms before the autocomplete menu appears
+
+    -- A comma-separated list of options for Insert mode completion
+    completeopt       = "menu,menuone,preview,noselect",
 
     -- bracket matching
-    showmatch = true, -- When a bracket is inserted, briefly jump to the matching one
-    matchtime = 2,    -- Tenths of a second to show the matching paren
-
+    showmatch         = true, -- When a bracket is inserted, briefly jump to the matching one
+    matchtime         = 2,    -- Tenths of a second to show the matching paren
 
     -- search settings
-    ignorecase    = true,  -- Ignore case in search patterns
-    smartcase     = true,  -- Override the 'ignorecase' option if the search
+    ignorecase        = true, -- Ignore case in search patterns
+    smartcase         = true, -- Override the 'ignorecase' option if the search
     -- pattern contains upper case characters
-    hlsearch      = false, -- When there is a previous search pattern,
+    hlsearch          = false, -- When there is a previous search pattern,
     -- highlight all its matches
-    incsearch     = true,  -- While typing a search command, show where the pattern,
+    incsearch         = true, -- While typing a search command, show where the pattern,
     -- as it was typed so far, matches
 
     -- colors
-    termguicolors = true,      -- Enables 24-bit RGB color in the Terminal UI
+    termguicolors     = true,  -- Enables 24-bit RGB color in the Terminal UI
     -- of screen columns that are highlighted
-    background    = "dark",    -- When set to "dark" or "light", adjusts the
+    background        = "dark", -- When set to "dark" or "light", adjusts the
     -- default color groups for that background type
-    winborder     = "rounded", -- Border type on floating windows
+    winborder         = "rounded", -- Border type on floating windows
 
-
-    -- A comma-separated list of options for Insert mode completion
-    completeopt   = "menu,menuone,noselect",
 
     -- This is a list of character encodings considered when starting to edit an existing file
     fileencodings = "utf8,big5,iso-2022-jp,euc-jp,cp932,default,latin1",
